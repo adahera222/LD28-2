8f28ea9a059677594a2af8846a43f54dbb6b6a26
Michael Delfino <megabeano@gmail.com>
2013-12-15 12:55:06 -0500

    Add enemies

--- ld28.py
+++ ld28.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import os.path
+import os.path, random
 
 # import basic pygame modules
 import pygame
 from pygame.locals import *
 
 # game constants
-SCREENRECT = Rect(0, 0, 1920, 1080)
-MAX_SHOTS  = 2
+SCREENRECT      = Rect(0, 0, 1920, 1080)
+MAX_SHOTS       = 1
+ENEMY_RELOAD    = 12
+ENEMY_ODDS      = 25
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -68,8 +70,28 @@ class Shot(pygame.sprite.Sprite):
         if self.rect.top <= 0:
             self.kill()
 
+class Enemy(pygame.sprite.Sprite):
+    speed = 10
+    animcycle = 12
+    images = []
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.facing = random.choice((-1,1)) * Enemy.speed
+        if self.facing < 0:
+            self.rect.right = SCREENRECT.right
+
+    def update(self):
+        self.rect.move_ip(self.facing, 0)
+        if not SCREENRECT.contains(self.rect):
+            self.facing = -self.facing;
+            self.rect.top = self.rect.bottom + 1
+            self.rect = self.rect.clamp(SCREENRECT)
+
 def main():
     pygame.init()
+    random.seed()
     if pygame.mixer and not pygame.mixer.get_init():
         print ('Warning, no sound')
         pygame.mixer = None
@@ -85,6 +107,7 @@ def main():
     img = load_image('player_ship.gif')
     Player.images = [img, pygame.transform.flip(img, 1, 0)]
     Shot.images = [load_image('shot.gif')]
+    Enemy.images = [load_image('enemy.gif')]
 
     pygame.display.set_caption('Pygame Aliens')
     pygame.mouse.set_visible(0)
@@ -98,19 +121,27 @@ def main():
     screen.blit(background, (0,0))
     pygame.display.flip()
 
+    enemies = pygame.sprite.Group()
     shots = pygame.sprite.Group()
     all = pygame.sprite.RenderUpdates()
 
     Player.containers = all
+    Enemy.containers = enemies, all
     Shot.containers = shots, all
 
+    # load the sound effects/music
+    explode_sound = load_sound('explode.wav')
     shoot_sound = load_sound('shoot1.wav')
     if pygame.mixer:
         music = os.path.join(main_dir, 'data', 'POL-rocket-station-short.wav')
         pygame.mixer.music.load(music)
         pygame.mixer.music.play(-1)
 
+    enemy_reload = ENEMY_RELOAD
+    kills = 0
+
     player = Player()
+    Enemy()
 
     while not game_over: #main game loop
         for event in pygame.event.get():
@@ -134,6 +165,24 @@ def main():
         player.reloading = firing
         player.move(direction)
         firing = keystate[K_SPACE]
+
+        # create new enemy
+        if enemy_reload:
+            enemy_reload -= 1
+        elif not int(random.random() * ENEMY_ODDS):
+            Enemy()
+            enemy_reload = ENEMY_RELOAD
+
+        # detect collisions
+        for enemy in pygame.sprite.spritecollide(player, enemies, 1):
+            explode_sound.play()
+            player.kill()
+            game_over = True
+
+        for enemy in pygame.sprite.groupcollide(shots, enemies, 1, 1).keys():
+            explode_sound.play()
+            #Explosion(alien)
+            #SCORE = SCORE + 1
 
         #draw the scene
         dirty = all.draw(screen)
